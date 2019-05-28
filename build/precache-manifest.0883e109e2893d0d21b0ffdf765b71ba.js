ebd2db7fe1bb530303e15fa45516a9feda875e20
wang guo qiang <805375226@qq.com>
2019-05-28 17:05:41 +0800

    react first

---
+++ build/precache-manifest.0883e109e2893d0d21b0ffdf765b71ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc9711ca8a8f8166701863c621565ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2608b8b609cb3d2c8a61",
+    "url": "/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "7bdf9f9c2cfef2a798e5",
+    "url": "/static/css/main.8564b87d.chunk.css"
+  },
+  {
+    "revision": "2608b8b609cb3d2c8a61",
+    "url": "/static/js/2.8e2b79cc.chunk.js"
+  },
+  {
+    "revision": "7bdf9f9c2cfef2a798e5",
+    "url": "/static/js/main.e3c0c9eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
